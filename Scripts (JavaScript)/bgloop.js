d01418a75eb7d1919c902f1d46c6daaafa49dd11
Aman Shahid <44910252+Epicwyvern@users.noreply.github.com>
2025-09-21 04:14:31 -0400

    Delete Scripts (JavaScript) directory

--- Scripts (JavaScript)/bgloop.js
+++
@@ -1,9 +0,0 @@
-(function back_loop () {
-    var vidElem = document.getElementById('back');
-    vidElem.addEventListener("timeupdate", function () {
-        if (vidElem.currentTime >= 9.7) {
-            vidElem.currentTime = 4.4;
-            vidElem.play();
-        }
-    }, false);
-})();
